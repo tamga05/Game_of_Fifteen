b91f803b5b6363d9a1746210392d94cde2567ba6
tamga05 <tamga05@gmail.com>
2023-01-31 14:07:25 +0300

    refactoring code

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ const cellSize = 100;
 
 const empty = {
     value: 0,
-    top: 0,
-    left: 0
+    bottom: 0,
+    right: 0
 };
 
 const cells = [];
@@ -17,27 +17,27 @@ function move(index) {
 
     const cell = cells[index];
 
-    const leftDiff = Math.abs(empty.left - cell.left);
-    const topDiff = Math.abs(empty.top - cell.top);
+    const rightDiff = Math.abs(empty.right - cell.right);
+    const bottomDiff = Math.abs(empty.bottom - cell.bottom);
 
-    if (leftDiff + topDiff > 1) {
+    if (rightDiff + bottomDiff > 1) {
         return;
     }
 
-    cell.element.style.left = `${empty.left * cellSize}px`;
-    cell.element.style.top = `${empty.top * cellSize}px`;
+    cell.element.style.right = `${empty.right * cellSize}px`;
+    cell.element.style.bottom = `${empty.bottom * cellSize}px`;
 
-    const emptyLeft = empty.left;
-    const emptyTop = empty.top;
+    const emptyRight = empty.right;
+    const emptyBottom = empty.bottom;
 
-    empty.left = cell.left;
-    empty.top = cell.top;
+    empty.right = cell.right;
+    empty.bottom = cell.bottom;
 
-    cell.left = emptyLeft;
-    cell.top = emptyTop;
+    cell.right = emptyRight;
+    cell.bottom = emptyBottom;
 
     const isFinished = cells.every(cell => {
-        return cell.value === (cell.top * 4 + (cell.left + 1));
+        return cell.value === (cell.bottom * 4 + (cell.right + 1));
     });
 
     if (isFinished) {
@@ -53,18 +53,18 @@ for (let i = 1; i <= 15; i++) {
     cell.className = 'cell';
     cell.innerHTML = value;
 
-    const left = i % 4;
-    const top = (i - left) / 4;
+    const right = i % 4;
+    const bottom = (i - right) / 4;
 
     cells.push({
         value: value,
-        left: left,
-        top: top,
+        right: right,
+        bottom: bottom,
         element: cell
     });
 
-    cell.style.left = `${left * cellSize}px`;
-    cell.style.top = `${top * cellSize}px`;
+    cell.style.right = `${right * cellSize}px`;
+    cell.style.bottom = `${bottom * cellSize}px`;
 
     field.append(cell);
 
